32392db11b71376f798495d0b788c879c4eed692
giuliana valarino <gvalarino2015@gmail.com>
2022-12-01 18:28:08 -0300

    agrego navbar y relacionados

---
+++ my-app/src/components/ItemListContainer/index.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles.css"
+
+
+const ItemListContainer =() => {
+    return(
+        <div className="body">
+            <div className="content-title"> <h2 className="title">Bienvenido/a a Chiquita</h2></div>
+            <div className="content-description">
+                <p>Puedes encontrar todos nuestros productos al mejor precio.</p>
+            </div>
+        </div>
+    )
+}
+
+export default ItemListContainer;
---
+++ my-app/src/components/Navbar/index.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import './styles.css';
+import CartWidget from "../CartWidget";
+
+const Navbar =() => {
+    return ( 
+        <header className="header-menu">
+            <a className="navbar-brand" href="#Inicio">Chiquita
+            </a>
+            <nav className="header-menu">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <a className="nav-link" href="#ACCESORIOS">
+                Nosotros
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#CEJAS">
+                Contacto
+              </a>
+            </li>
+          </ul>
+            <CartWidget/>
+            </nav>
+        </header>
+    )
+}
+
+export default Navbar;
