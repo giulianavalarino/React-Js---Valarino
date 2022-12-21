14a1ea219968cb9ac5f7f9a64c95ef274677199f
giuliana valarino <gvalarino2015@gmail.com>
2022-12-21 08:43:14 -0300

    Corrección estilos

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,19 +5,28 @@ import Navbar from './components/Navbar';
 import ItemListContainer from './components/ItemListContainer';
 import Sidebar from './components/Sidebar';
 import Button from './components/Button';
+import {PRODUCTS} from './components/constants/data/products';
+import Card from './components/Card';
 
 function App() {
   const [isOpen, setIsOpen] = useState(false);
+  const[showProducts, setShowProducts] = useState(false);
   const onHandlerClick = () => {
     setIsOpen(!isOpen);
+    setShowProducts(!showProducts);
   }
   return (
     <div className="App">
         <Navbar/>
         <Sidebar onClose={onHandlerClick} isOpen={isOpen}>
-          <h2>Item</h2>
         </Sidebar>
         <Button text='Click' onHandlerClick={onHandlerClick}/>
+        {showProducts}
+        <div className='products-container'>
+        {PRODUCTS.map((product) =>(
+          <Card product={product} key={product.name}/>
+        ))}
+        </div>
         <ItemListContainer/>
     </div>
   );
---
+++ my-app/src/components/constants/data/products.js
@@ -0,0 +1,29 @@
+export const PRODUCTS = [
+    {
+    id: 1, 
+    categoryId: 1, 
+    description: 'Remera de algodón, de manga corta con estampado de hojas. ', 
+    image: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcSwjbEq3wFX9Phxrrxw_dXTURTaEGPlSNP2TaIQLwOiaGsyWqJ6q9cJysfmD2GH2bqDlrk2BujDQw&usqp=CAc', 
+    name: 'Remera estampado hojas', 
+    price: 50, 
+    stock: 31,
+    },
+    {
+        id: 2,
+        categoryId: 2,
+        description: 'Campera deportiva fleixble',
+        image: 'https://f.fcdn.app/imgs/9257b1/www.lacancha.uy/lcanuy/084b/webp/catalogo/HA2297_HA2297_1/800x800/campera-adidas-terrex-multi-primegreen-campera-adidas-terrex-multi-primegreen.jpg',
+        name: 'Campera',
+        price: 67,
+        stock: 4,
+    },
+    {
+        id: 3,
+        categoryId: 3,
+        description: 'Canguro Básico',
+        image: 'https://f.fcdn.app/imgs/bec1f5/www.timeout.com.uy/timeuy/729c/webp/catalogo/001MH106_00K_1/800x800/canguro-basico-otros-hombre-00k.jpg',
+        name: 'Canguro',
+        price: 34,
+        stock: 6,
+    }
+]
