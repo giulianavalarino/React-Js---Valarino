ca91ebc9a6e79605c161da59f6b99b0ebcac8ae9
giuliana valarino <gvalarino2015@gmail.com>
2022-12-30 12:05:55 -0300

    agregué imagen al itemDetalles

--- my-app/src/components/itemDetalles/itemDetalles.jsx
+++ my-app/src/components/itemDetalles/itemDetalles.jsx
@@ -6,14 +6,15 @@ export const ItemDetalles = ({arregloItems}) => {
 
     const {id} = useParams();
 
-    const {nombre, descripcion, numero} = arregloItems[id - 1];
+    const {nombre, descripcion, numero, imagen} = arregloItems[id - 1];
 
     return(
         <>
-      <div className="containter text-center">
+      <div className="detalles containter text-center">
         <h1>{nombre}</h1>
         <h2>{descripcion}</h2>
         <h3>{numero}</h3>
+        <img src={imagen}></img>
       </div>
         </>
     );
